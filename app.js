695467a5f417b0c166c62b7b667b2fa2f5c676cf
Krsto Culafic <kculafic@gmail.com>
2018-05-01 13:54:11 -0500

    Complete basic challenge requirements using Vanilla JS

---
+++ app.js
@@ -0,0 +1,93 @@
+var pomodoro = {
+  started: false,
+  minutes: 0,
+  seconds: 0,
+  interval: null,
+  minutesDom: null,
+  secondsDom: null,
+
+  init: function() {
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+
+    this.interval = setInterval(function(){
+      self.intervalCountdown.apply(self);
+    }, 1000);
+
+    document.querySelector('#start').onclick = function(){
+      console.log('timer started');
+      self.startWork.apply(self);
+    }
+
+    document.querySelector('#pause').onclick = function(){
+      self.pauseTimer.apply(self);
+    };
+
+    document.querySelector('#reset').onclick = function(){
+      self.resetTimer.apply(self);
+    }
+  },
+
+  resetVariables: function(mins, secs, started){
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+  },
+
+  startWork: function() {
+    this.resetVariables(25, 0, true);
+  },
+
+  pauseTimer: function(){
+    if (this.started === true) {
+      this.started = false;
+    } else if (this.started === false) {
+      this.started = true;
+    }
+
+    this.updateDom();
+  },
+
+  resetTimer: function(){
+    console.log('reset clicked');
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+
+  toDoubleDigit : function(num){
+    if(num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+
+  updateDom : function(){
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+  },
+
+  intervalCountdown : function(){
+    if(!this.started) return false;
+    if(this.seconds == 0) {
+      if(this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+
+  timerComplete : function(){
+    this.started = false;
+    window.alert('Timer complete!')
+  }
+};
+
+window.onload = function(){
+  pomodoro.init();
+};
